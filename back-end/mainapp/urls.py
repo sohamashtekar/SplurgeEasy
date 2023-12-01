4a6f6247c39f64580c4e4ad4b0c6e6eb65215691
DESKTOP-VQ9S7D2\Panda <sohamashtekar31@gmail.com>
2023-12-01 16:44:50 -0500

    App name change imports and comments

--- back-end/mainapp/urls.py
+++ back-end/mainapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SEBackEnd project.
+URL configuration for mainapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
